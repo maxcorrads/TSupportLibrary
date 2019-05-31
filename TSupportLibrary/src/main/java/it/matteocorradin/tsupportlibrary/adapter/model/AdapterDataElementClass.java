05e8d899f1a080e493f53f9330bcf2ad0b15e6b1
matteocorradin <maxcorrads@gmail.com>
2019-05-31 23:12:26 +0200

    ADET hyp

--- TSupportLibrary/src/main/java/it/matteocorradin/tsupportlibrary/adapter/model/AdapterDataElementClass.java
+++ TSupportLibrary/src/main/java/it/matteocorradin/tsupportlibrary/adapter/model/AdapterDataElementClass.java
@@ -1,32 +1,51 @@
 package it.matteocorradin.tsupportlibrary.adapter.model;
 
-import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class AdapterDataElementClass {
 
-    private static final ArrayList<String> elements
-            = new ArrayList<>();
+    private static class ADETWithPosition{
+        private final IAdapterDataElementType adet;
+        private final int position;
 
-    public static int addADET(String adet){
-        if (!elements.contains(adet)){
-            elements.add(adet);
+        private ADETWithPosition(IAdapterDataElementType adet, int position) {
+            this.adet = adet;
+            this.position = position;
         }
-        return elements.indexOf(adet);
+
+        public IAdapterDataElementType getAdet() {
+            return adet;
+        }
+
+        public int getPosition() {
+            return position;
+        }
+
+    }
+
+    private static final ConcurrentHashMap<String, ADETWithPosition> elements
+            = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<Integer, ADETWithPosition> elementsPos
+            = new ConcurrentHashMap<>();
+
+    public synchronized static int addADET(IAdapterDataElementType adet){
+        ADETWithPosition element;
+        if (elements.containsKey(adet.getName())) {
+            element = elements.get(adet.getName());
+        } else {
+            int pos = elements.size();
+            element = new ADETWithPosition(adet, pos);
+            elements.put(adet.getName(), element);
+            elementsPos.put(pos, element);
+        }
+        assert element != null;
+        return element.position;
     }
 
     public static IAdapterDataElementType getADET(final int n){
-        final String adet = elements.get(n);
-        return new IAdapterDataElementType() {
-            @Override
-            public String getName() {
-                return adet;
-            }
-
-            @Override
-            public int getId() {
-                return n;
-            }
-        };
+        ADETWithPosition element = elementsPos.get(n);
+        assert element != null;
+        return element.adet;
     }
 
 }
--- TSupportLibrary/src/main/java/it/matteocorradin/tsupportlibrary/adapter/model/IAdapterDataElementType.java
+++ TSupportLibrary/src/main/java/it/matteocorradin/tsupportlibrary/adapter/model/IAdapterDataElementType.java
@@ -1,6 +1,9 @@
 package it.matteocorradin.tsupportlibrary.adapter.model;
 
+import it.matteocorradin.tsupportlibrary.adapter.AdapterDataViewHolderAbstractFactory;
+
 public interface IAdapterDataElementType {
     String getName();
     int getId();
+    AdapterDataViewHolderAbstractFactory getFactory();
 }
